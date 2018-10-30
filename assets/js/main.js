1f191f35eb1bf131cd418d69c92f06b836e01c20
Alecto <fomenkoandrey@inbox.ru>
2018-10-30 20:55:11 +0200

    замена библиотек bower на yarn. обновление сборщика проекта gulpfile.js для gulp 4.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 
 $(document).ready(function () {
-  $('.header-navbar__hamburger').click(function(){
+
+  $('.header-navbar__hamburger').on('click', function () {
     $(this).toggleClass('active');
     $('.header-navbar__list').toggleClass('active');
   });
+
 });
