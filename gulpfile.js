393821a71c42ab01757a0af53c1a31c441421e97
Alecto <fomenkoandrey@inbox.ru>
2018-10-31 01:11:12 +0200

    замена библиотек bower на yarn. незначительные правки стартового шаблона. обновление сборщика проекта gulpfile.js для gulp 4.

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,52 @@
 'use strict';
 
-var gulp             = require('gulp');
-var sass             = require('gulp-sass');
-var autoprefixer     = require('gulp-autoprefixer');
-var gcmq             = require('gulp-group-css-media-queries');
-var sourcemaps       = require('gulp-sourcemaps');
-var notify           = require('gulp-notify');
-var browserSync      = require('browser-sync');
-var smartgrid        = require('smart-grid');
-var csscomb          = require('gulp-csscomb');
-var gcmq             = require('gulp-group-css-media-queries');
-var runSequence      = require('run-sequence');
-var changed          = require('gulp-changed');
+require('../../gulp/gulp-init.js');
 
+const
+  SCSS = './assets/scss',
+  CSS  = './assets/css',
+  HTML = '.',
+  JS   = './assets/js';
 
-const path = {
-  scssFolder:     './assets/scss/',
-  scssFiles:      './assets/scss/**/*.scss',
-  scssTempFolder: './temp/scss/',
-  scssTempFiles:  './temp/scss/**/*.scss',
-  cssFolder:      './assets/css/',
-  cssFiles:       './assets/css/**/*.css',
-  jsFiles:        './assets/js/**/*.js',
-  phpFiles:       './theme/**/*.php',
-  htmlFiles:      './*.html',
-  distFolder:     './dist/',
-  tempFolder:     './temp/',
-  uploadFolder:   './upload/'
+global.$.path = {
+  scss: {
+    folder: SCSS + '/',
+    files: SCSS + '/**/*.scss',
+    combFolder: SCSS + '-comb/',
+    combFiles: SCSS + '-comb/**/*.scss'
+  },
+  css: {
+    folder: CSS + '/',
+    files: CSS + '/**/*.css',
+    filesMin: CSS + '/**/*.min.css',
+    mapFolder: CSS + '/',
+    mapFiles: CSS + '/**/*.map'
+  },
+  html: {
+    folder: HTML + '/',
+    files: HTML + '/**/*.html'
+  },
+  js: {
+    folder: JS + '/',
+    files: JS + '/**/*.js',
+    filesMin: JS + '/**/*.min.js'
+  }
 };
 
+const { comb, combUpdate, combDelete }   = require('../../gulp/tasks/comb.js'),
+  scss     = require('../../gulp/tasks/scss.js'),
+  mincss   = require('../../gulp/tasks/mincss.js'),
+  uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
+  { sync, syncInit } = require('../../gulp/tasks/sync.js');
 
-gulp.task('comb', function () {
-  return gulp.src([path.scssFiles, '!' + path.scssFolder + '**/_smart-grid.scss'])
-    .pipe(csscomb('.csscomb.json').on("error", notify.onError(function (error) {
-      return "File: " + error.message;
-    })))
-    .pipe(gulp.dest(path.scssFolder));
-});
+function watchFiles () {
+  syncInit();
+  watch($.path.scss.files, series(comb, scss, combDelete, mincss));
+  watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
+  watch($.path.html.files, sync);
+}
 
-
-gulp.task('sass', function () {
-  return gulp.src(path.scssFiles)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on("error", notify.onError(function (error) {
-      return "File: " + error.message;
-    })))
-    .pipe(gcmq())
-    .pipe(autoprefixer({
-      browsers: ['last 5 versions', '> 1%'],
-      cascade: true
-    }))
-    .pipe(notify({
-      message: 'Compiled!',
-      sound: false
-    }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(path.cssFolder))
-    .pipe(browserSync.reload({stream: true}));
-});
-
-
-gulp.task('compile', function (callback) {
-  runSequence('comb', 'sass', callback);
-});
-
-
-gulp.task('browser-sync', function () {
-  browserSync({
-    server: {
-      baseDir: './'
-    },
-    notify: false
-  })
-});
-
-
-gulp.task('watch', ['browser-sync', 'compile'], function () {
-  gulp.watch(path.scssFiles, ['compile']);
-  gulp.watch(path.htmlFiles, browserSync.reload);
-  gulp.watch(path.jsFiles, browserSync.reload);
-  gulp.watch(path.cssFiles, browserSync.reload);
-});
+task('combScss', combUpdate);
+task('uglifyEs6', series(uglifyes, sync));
+task('sass2minCss', series(comb, scss, combDelete, mincss));
+task('watch', watchFiles);
