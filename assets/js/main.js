c8796c3374a8d3a15b27a51b72ab12dae1d18146
Alecto <fomenkoandrey@inbox.ru>
2018-12-23 22:47:27 +0200

    исправление бага логики мобильного меню + оптимизация кода меню.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
   $('.header-navbar__hamburger').on('click', function () {
     $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+    $('.header-navbar__list').stop(true, true).fadeToggle(500);
   });
 
 });
