0e084cce62a8ca20f0d2739ddcabf21ede0af11c
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-07-05 10:48:40 +0300

    Обновление структуры SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
-require('../../gulp/gulp-init.js')();
+require('../../gulp/gulp-init.js')({HTML: '.'});
 
 const comb = require('../../gulp/tasks/comb.js'),
-  scss     = require('../../gulp/tasks/scss.js').scss,
-  mincss   = require('../../gulp/tasks/mincss.js'),
-  uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
-  { sync, syncInit } = require('../../gulp/tasks/sync.js');
+    scssDC     = require('../../gulp/tasks/scss.js').scssDC,
+    mincss   = require('../../gulp/tasks/mincss.js'),
+    uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
+    { sync, syncInit } = require('../../gulp/tasks/sync.js');
 
 function watchFiles () {
-  syncInit();
-  watch($.path.scss.files, series(scss, mincss));
-  watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
-  watch($.path.html.files, sync);
+    syncInit();
+    watch($.path.scss.files, series(scssDC, mincss));
+    watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
+    watch($.path.html.files, sync);
 }
 
 task('combScss', comb);
 task('uglifyEs6', series(uglifyes, sync));
-task('sass', series(scss));
+task('sass', series(scssDC));
 task('watch', watchFiles);
