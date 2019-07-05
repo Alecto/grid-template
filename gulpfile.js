a87f8a1b980990e6bfb9b64d539f7f6445a647ba
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-07-05 10:42:52 +0300

    Из css удалены комментарии.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,19 @@
 require('../../gulp/gulp-init.js')({HTML: '.'});
 
 const comb = require('../../gulp/tasks/comb.js'),
-    scss     = require('../../gulp/tasks/scss.js').scss,
+    scssDC     = require('../../gulp/tasks/scss.js').scssDC,
     mincss   = require('../../gulp/tasks/mincss.js'),
     uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
     { sync, syncInit } = require('../../gulp/tasks/sync.js');
 
 function watchFiles () {
     syncInit();
-    watch($.path.scss.files, series(scss, mincss));
+    watch($.path.scss.files, series(scssDC, mincss));
     watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
     watch($.path.html.files, sync);
 }
 
 task('combScss', comb);
 task('uglifyEs6', series(uglifyes, sync));
-task('sass', series(scss));
+task('sass', series(scssDC));
 task('watch', watchFiles);
