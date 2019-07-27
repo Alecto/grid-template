77222b659be2701a5829a322f01900ce916c1b0a
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-07-27 22:00:09 +0300

    Ветка для классной верстки, до использования препроцессорного стиля.

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('.header-navbar__hamburger').on('click', function () {
+    $(this).toggleClass('active');
+    $('.header-navbar__list').
+      stop(true, true).
+      fadeToggle(500);
+  });
+});
