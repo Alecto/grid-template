77643d3cb469eac9657aa1be2bf866e707822d1c
Alecto <fomenkoandrey1978@gmail.com>
2020-04-13 15:41:00 +0300

    оптимизации для урока.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('header-navbar__btn--active');
     $('.header-navbar__list').
       stop(true, true).
       fadeToggle(500);
