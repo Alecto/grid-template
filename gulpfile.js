4005ceade90550dcc476e1a683463e0f0bd0805d
Alecto <fomenkoandrey1978@gmail.com>
2020-07-30 09:41:19 +0300

    обновление стартового шаблона.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,84 @@
-'use strict';
+const {task, series, parallel, src, dest, watch} = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const notify = require('gulp-notify');
+const postcss = require('gulp-postcss');
+const csscomb = require('gulp-csscomb');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const sortCSSmq = require('sort-css-media-queries');
 
-require('../../gulp/gulp-init.js')({HTML: '.'});
+const path = {
+  scssFolder: './assets/scss/',
+  scssFiles: './assets/scss/**/*.scss',
+  scssFile: './assets/scss/style.scss',
+  cssFolder: './assets/css/',
+  cssFile: './assets/css/style.css',
+  htmlFiles: './*.html',
+  jsFiles: './assets/js/**/*.js'
+};
 
-const comb = require('../../gulp/tasks/comb.js'),
-    scssDC     = require('../../gulp/tasks/scss.js').scssDC,
-    mincss   = require('../../gulp/tasks/mincss.js'),
-    uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
-    { sync, syncInit } = require('../../gulp/tasks/sync.js');
+const plugins = [
+  autoprefixer({
+    overrideBrowserslist: [
+      'last 5 versions',
+      '> 1%'
+    ],
+    cascade: true
+  }),
+  mqpacker({sort: sortCSSmq})
+];
 
-function watchFiles () {
-    syncInit();
-    watch($.path.scss.files, series(scssDC, mincss));
-    watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
-    watch($.path.html.files, sync);
+function scss() {
+  return src(path.scssFile).
+    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
+    pipe(postcss(plugins)).
+    pipe(dest(path.cssFolder)).
+    pipe(notify({
+      message: 'Compiled!',
+      sound: false
+    })).
+    pipe(browserSync.reload({stream: true}));
 }
 
-task('combScss', comb);
-task('uglifyEs6', series(uglifyes, sync));
-task('sass', series(scssDC));
+function scssDev() {
+  return src(path.scssFile, {sourcemaps: true}).
+    pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)).
+    pipe(postcss(plugins)).
+    pipe(dest(path.cssFolder, {sourcemaps: true})).
+    pipe(notify({
+      message: 'Compiled!',
+      sound: false
+    })).
+    pipe(browserSync.reload({stream: true}));
+}
+
+function comb() {
+  return src(path.scssFiles).
+    pipe(csscomb()).
+    on('error', notify.onError((error) => `File: ${error.message}`)).
+    pipe(dest(path.scssFolder));
+}
+
+function syncInit() {
+  browserSync({
+    server: {baseDir: './'},
+    notify: false
+  });
+}
+
+async function sync() {
+  browserSync.reload();
+}
+
+function watchFiles() {
+  syncInit();
+  watch(path.scssFiles, series(scss));
+  watch(path.htmlFiles, sync);
+  watch(path.jsFiles, sync);
+}
+
+task('comb', series(comb));
+task('scss', series(scss));
+task('dev', series(scssDev));
 task('watch', watchFiles);
