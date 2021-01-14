fca2a5289100f0f44dda03c563facc793efed081
Alecto <fomenkoandrey1978@gmail.com>
2021-01-14 10:55:42 +0200

    grid-gap -> gap + оптимизации имен классов.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 
-$(document).ready(function () {
+$(document).ready(() => {
 
   $('.header-navbar__btn').on('click', function () {
     $(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').stop(true, true).fadeToggle(500);
+    $('.header-navbar__list')
+      .stop(true, true)
+      .slideToggle(500);
   });
 
 });
