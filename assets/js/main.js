a13c91a5c700ff01c07f41e8bad1df28cd5cbcf8
Alecto <fomenkoandrey1978@gmail.com>
2021-01-14 11:39:54 +0200

    оптимизации имен классов, добавление примеров с медиа.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+$(document).ready(() => {
   $('.header-navbar__btn').on('click', function () {
     $(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').
-      stop(true, true).
-      fadeToggle(500);
+    $('.header-navbar__list')
+      .stop(true, true)
+      .fadeToggle(500);
   });
 });
